eefda358c0ed6b7559c09c4ec060b2a51231cf90
Umangi54 <32841547+Umangi54@users.noreply.github.com>
2018-11-17 16:22:12 +0530

    Update IQKeyboardManager.h

--- IQKeyboardManager/IQKeyboardManager.h
+++ IQKeyboardManager/IQKeyboardManager.h
@@ -21,6 +21,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+//Usefull librabry
 #import "IQKeyboardManagerConstants.h"
 #import "IQUIView+IQKeyboardToolbar.h"
 #import "IQPreviousNextView.h"
